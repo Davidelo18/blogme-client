e21f0690a85c7f5baea0291118f6e5a0d9e3af62
Dawid <daw.kielbik@gmail.com>
2021-07-23 01:47:04 +0200

    Wielopoziomowe menu glowne na mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,12 @@ function Header() {
                 <button className="header__burger" id="burgerBtn"></button>
                 <ul className="header__options-container" id="menuList">
                     <li className="header__option">Mój profil</li>
-                    <li className="header__option">Opcje</li>
+                    <li className="header__option" id="optionsMenu">Opcje</li>
+                    <li className="header__hidden-menu-container" id="menuFor_optionsMenu">
+                        <ul className="header__options-container header__options-container--hidden">
+                            <li className="header__option">Tryb nocny</li>
+                        </ul>
+                    </li>
                     <li className="header__option">Wyloguj się</li>
                 </ul>
             </nav>
@@ -24,6 +29,18 @@ window.addEventListener('DOMContentLoaded', () => {
     $burgerBtn.addEventListener('click', () => {
         $burgerBtn.classList.toggle('menu-opened');
         $menuList.classList.toggle('open');
+    });
+
+    const $optionsMenu = document.getElementById('optionsMenu');
+
+    $optionsMenu.addEventListener('click', () => {
+        const $menuForOptionsMenu = document.getElementById(`menuFor_optionsMenu`);
+        const numberOfChildren = $menuForOptionsMenu.querySelector('ul').childElementCount;
+
+        $menuForOptionsMenu.classList.toggle('open');
+
+        if ($menuForOptionsMenu.classList.contains('open')) $menuForOptionsMenu.style.maxHeight = `${numberOfChildren * 39}px`;
+        else $menuForOptionsMenu.style.maxHeight = 0;
     });
 });
 
