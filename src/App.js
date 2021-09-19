f747a8c9b2c8c33731ac6215352ba1c4067e9ec0
Dawid <daw.kielbik@gmail.com>
2021-09-19 23:27:31 +0200

    Profile userów

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/root.scss';
 
 import { AuthProvider } from './core/auth';
@@ -8,6 +8,7 @@ import { AuthRoute, NotLoginAuth } from './core/authRoute';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import SinglePost from './pages/SinglePost';
+import SingleUser from './pages/SingleUser';
 
 import Header from './components/Header';
 
@@ -18,7 +19,8 @@ function App() {
         <Header/>
         <NotLoginAuth exact path="/" component={Home}/>
         <AuthRoute exact path="/login" component={Login}/>
-        <Route exact path="/wpis/:postId" component={SinglePost}/>
+        <NotLoginAuth exact path="/wpis/:postId" component={SinglePost}/>
+        <NotLoginAuth exact path="/user/:username" component={SingleUser}/>
       </Router>
     </AuthProvider>
   );
